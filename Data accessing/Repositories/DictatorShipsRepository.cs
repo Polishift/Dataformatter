dae0c5abb28f451b3f25811e49cadfd8964c45bd
RobertKraaijeveld <0890289@hr.nl>
2017-12-02 16:21:30 +0100

    merged dictatorship feature

---
+++ Data accessing/Repositories/DictatorShipsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Dataformatter.Datamodels;
+using Dataformatter.Dataprocessing.Parsing;
+
+namespace Dataformatter.Data_accessing.Repositories
+{
+    public class DictatorShipsRepository : IRepository<DictatorshipEntity>
+    {
+        private static readonly DictatorshipEntity[] AllDictatorships = 
+            JsonReader<DictatorshipEntity>.ParseJsonToListOfObjects("Dictatorships.JSON");
+
+        public DictatorshipEntity[] GetAll()
+        {
+            return AllDictatorships;
+        }
+
+        public DictatorshipEntity[] GetByCountry(string countryCode)
+        {
+            return AllDictatorships.Where(d => d.CountryCode == countryCode).ToArray();
+        }
+    }
+}
---
+++ Dataprocessing/Entities/DictatorshipEntity.cs
@@ -0,0 +1,19 @@
+﻿using Dataformatter.Dataprocessing.Entities;
+
+namespace Dataformatter.Datamodels
+{
+    public class DictatorshipEntity : IEntity, ICountryRuler
+    {
+        public string CountryCode { get; set; }
+		public string Name { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+        public string PartyClassification { get; set; }
+
+		public override string ToString()
+        {
+            return Name + ", which is '" + PartyClassification + "' " +  
+                   " has ruled since " + From;
+        }
+    }
+}
